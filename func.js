00f4007633cbb350c9193bad65d39cc35563eec8
janexmgd <janexmgd@gmail.com>
2025-07-26 14:45:42 +0700

    now using all category to get live list

--- func.js
+++ func.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-export const getJkt48Live = async () => {
+export const getIdnLive = async (category) => {
   try {
     const { data } = await axios.get('https://api.idn.app/api/v4/livestreams', {
       params: {
-        category: 'jkt48',
+        category: category,
         page: '1',
       },
       headers: {
@@ -12,6 +12,7 @@ export const getJkt48Live = async () => {
         'x-api-key': '123f4c4e-6ce1-404d-8786-d17e46d65b5c',
       },
     });
+
     return data;
   } catch (error) {
     throw error;
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import streamifier from 'streamifier';
-import { getJkt48Live } from './func.js';
+import { getIdnLive } from './func.js';
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 
-server.get('/live-jkt48', async (req, res) => {
+server.get('/list-stream', async (req, res) => {
   try {
-    const data = await getJkt48Live();
+    const { category } = req.query;
+    const data = await getIdnLive(category);
     const liveData = data.data;
     const filtered = liveData.filter((item) => item.status == 'live');
     return res.status(200).json({
@@ -22,20 +23,14 @@ server.get('/live-jkt48', async (req, res) => {
     return res.status(500).json({ message: 'failed get live' });
   }
 });
-server.get('/stream-live', async (req, res) => {
+server.get('/stream', async (req, res) => {
   try {
-    const userAgent =
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36';
     const { url } = req.query;
     if (!req.query?.url) {
       return res.status(400).json({ message: 'invalid query or unkown' });
     }
     const response = await fetch(url, {
       method: 'GET',
-      headers: {
-        'User-Agent': 'Android/13/Redmi Note 8/6.51.0',
-        'x-api-key': '123f4c4e-6ce1-404d-8786-d17e46d65b5c',
-      },
     });
     if (!response.ok) {
       console.log(response);
